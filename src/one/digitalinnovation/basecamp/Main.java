b40f421b6ced5a25f9ab61c0b196c92c43b1c8cb
thiagoleitedio@gmail.com <thiagoleitedio@gmail.com>
2021-09-10 19:06:52 -0300

    share

---
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+
+    }
+}
---
+++ src/one/digitalinnovation/basecamp/Quadrilatero.java
@@ -0,0 +1,28 @@
+package one.digitalinnovation.basecamp;
+
+/**
+ * Classe de exemplo para o exercício da Aula 2 de Métodos.
+ */
+public class Quadrilatero {
+
+    public static void area(double lado) {
+
+        System.out.println("Área do quadrado:" + lado * lado);
+    }
+
+    public static void area(double lado1, double lado2) {
+
+        System.out.println("Área do retângulo:" + lado1 * lado2);
+    }
+
+    public static void area(double baseMaior, double baseMenor, double altura) {
+
+        System.out.println("Área do trapézio:" + ((baseMaior+baseMenor)*altura) / 2);
+    }
+
+    public static void area(float diagonal1, float diagonal2) {
+
+        System.out.println("Área do losango:" + (diagonal1 * diagonal2)/2);
+    }
+
+}
